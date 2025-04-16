48f2af4677b14b2273eceb93cc85d2258c73e801
Anuja Dassanayake <anuja.2019646@iit.ac.lk>
2025-04-16 17:18:29 +0530

    debugging

--- aggregator-service/app/services.py
+++ aggregator-service/app/services.py
@@ -2,7 +2,7 @@ import httpx
 from typing import List
 from . import schemas
 
-INTEGRATION_SERVICE_URL = "http://localhost:8000/sales/"  # Change in prod
+INTEGRATION_SERVICE_URL = "http://integration-service/sales/"
 
 async def fetch_sales_data() -> List[schemas.Sale]:
     async with httpx.AsyncClient() as client:
